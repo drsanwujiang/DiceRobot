07ebf7748435a4d31d1509cdfae8197217ae382d
Drsanwujiang <drsanwujiang@gmail.com>
2020-11-05 23:06:38 +0800

    Add web APIs check

--- src/Server.php
+++ src/Server.php
@@ -108,58 +108,65 @@ class Server
      */
     public function route(Request $request, Response $response): void
     {
-        if (!preg_match("/^DiceRobot Panel\/[1-9]\.[0-9]\.[0-9]$/", $request->header["user-agent"] ?? "")) {
+        $method = $request->server["request_method"] ?? "";
+        $uri = $request->server["request_uri"] ?? "";
+        $content = (string) ($request->getContent() ?? "");
+
+        // Mirai APIs
+        if ($method == "POST") {
+            if ($uri == "/report") {
+                $this->report($content, $response);
+            } elseif ($uri == "/heartbeat") {
+                $this->heartbeat($response);
+            }
+        }
+
+        // Check header
+        if (!preg_match("/^DiceRobot Panel\/[1-9]\.[0-9]\.[0-9]$/", $request->header["x-dr-client"] ?? "")) {
             $this->notFound($response);
 
             return;
         }
 
-        $requestMethod = $request->server["request_method"] ?? "";
-        $requestUri = $request->server["request_uri"] ?? "";
-        $content = (string) ($request->getContent() ?? "");
-
-        if ($requestMethod == "OPTIONS") {
+        // Web APIs
+        if ($method == "OPTIONS") {
             $this->preflight($response);
-        } elseif ($requestMethod == "POST") {
-            if ($requestUri == "/report") {
-                $this->report($content, $response);
-            } elseif ($requestUri == "/heartbeat") {
-                $this->heartbeat($response);
-            } elseif ($requestUri == "/config") {
+        } elseif ($method == "POST") {
+            if ($uri == "/config") {
                 $this->setConfig($content, $response);
             } else {
                 $this->notFound($response);
             }
-        } elseif ($requestMethod == "GET") {
-            if ($requestUri == "/connect") {
+        } elseif ($method == "GET") {
+            if ($uri == "/connect") {
                 $this->connect($response);
-            } elseif ($requestUri == "/profile") {
+            } elseif ($uri == "/profile") {
                 $this->profile($response);
-            } elseif ($requestUri == "/status") {
+            } elseif ($uri == "/status") {
                 $this->status($response);
-            } elseif ($requestUri == "/statistics") {
+            } elseif ($uri == "/statistics") {
                 $this->statistics($response);
-            } elseif ($requestUri == "/config") {
+            } elseif ($uri == "/config") {
                 $this->config($response);
-            } elseif ($requestUri == "/pause") {
+            } elseif ($uri == "/pause") {
                 $this->pause($response);
-            } elseif ($requestUri == "/run") {
+            } elseif ($uri == "/run") {
                 $this->run($response);
-            } elseif ($requestUri == "/reload") {
+            } elseif ($uri == "/reload") {
                 $this->reload($response);
-            } elseif ($requestUri == "/stop") {
+            } elseif ($uri == "/stop") {
                 $this->stop($response);
-            } elseif ($requestUri == "/restart") {
+            } elseif ($uri == "/restart") {
                 $this->restart($response);
-            } elseif ($requestUri == "/update") {
+            } elseif ($uri == "/update") {
                 $this->update($response);
-            } elseif ($requestUri == "/mirai/status") {
+            } elseif ($uri == "/mirai/status") {
                 $this->miraiStatus($response);
-            } elseif ($requestUri == "/mirai/start") {
+            } elseif ($uri == "/mirai/start") {
                 $this->startMirai($response);
-            } elseif ($requestUri == "/mirai/stop") {
+            } elseif ($uri == "/mirai/stop") {
                 $this->stopMirai($response);
-            } elseif ($requestUri == "/mirai/restart") {
+            } elseif ($uri == "/mirai/restart") {
                 $this->restartMirai($response);
             } else {
                 $this->notFound($response);
