7f5a2df8c0ad6147a1dafe57ea4e2262fc6270e4
Drsanwujiang <drsanwujiang@gmail.com>
2020-12-01 21:43:27 +0800

    Fix startup bug

--- src/Server.php
+++ src/Server.php
@@ -66,8 +66,8 @@ class Server
 
         $this->logger->debug("Server created.");
 
-        // Application successfully initialized
-        if ($this->app->getStatus()->equals(AppStatusEnum::HOLDING()) && $this->setServer()) {
+        // Application just created or successfully initialized
+        if ($this->app->getStatus()->greaterThan(AppStatusEnum::RUNNING()) && $this->setServer()) {
             $this->setSignals();
             $this->setRoutes();
 
