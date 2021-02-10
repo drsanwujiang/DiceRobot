b743dacef6b3cc535eebc7e6961b6a9c872ec338
drsanwujiang <drsanwujiang@gmail.com>
2021-02-10 15:22:50 +0800

    Update test skeleton

--- tests/skeleton/config/config.php
+++ tests/skeleton/config/config.php
@@ -4,15 +4,17 @@ date_default_timezone_set("Asia/Shanghai");
 
 $settings = [];
 
+/** Directories */
 $settings["root"] = dirname(__DIR__);
 $settings["config"] = $settings["root"] . "/config";
+$settings["log"]["path"] = $settings["root"] . "/logs";
 $settings["data"]["root"] = $settings["root"] . "/data";
 $settings["data"]["card"] = $settings["data"]["root"] . "/card";
 $settings["data"]["chat"] = $settings["data"]["root"] . "/chat";
 $settings["data"]["deck"] = $settings["data"]["root"] . "/deck";
 $settings["data"]["reference"] = $settings["data"]["root"] . "/reference";
 $settings["data"]["rule"] = $settings["data"]["root"] . "/rule";
-$settings["log"]["path"] = $settings["root"] . "/logs";
+$settings["mirai"]["path"] = dirname($settings["root"]) . "/mirai";
 
 require __DIR__ . "/custom_config.php";
 
