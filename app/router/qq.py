4a4dc59fb1f735cbfe44c996acad6a54831ddd34
Drsanwujiang <drsanwujiang@gmail.com>
2024-09-06 00:53:14 +0800

    Optimize code

--- app/router/qq.py
+++ app/router/qq.py
@@ -14,6 +14,7 @@ async def get_status() -> JSONResponse:
     logger.info("QQ manage request received: get status")
 
     return JSONResponse(data={
+        "downloading": qq_manager.is_downloading(),
         "downloaded": qq_manager.is_downloaded(),
         "installing": qq_manager.is_installing(),
         "installed": qq_manager.is_installed(),
@@ -25,7 +26,9 @@ async def get_status() -> JSONResponse:
 async def download() -> JSONResponse:
     logger.info("QQ manage request received: download")
 
-    if qq_manager.is_downloaded():
+    if qq_manager.is_downloading():
+        raise BadRequestError(message="QQ DEB file is downloading")
+    elif qq_manager.is_downloaded():
         raise BadRequestError(message="QQ DEB file already downloaded")
 
     qq_manager.download()
@@ -39,8 +42,9 @@ async def install() -> JSONResponse:
 
     if not qq_manager.is_downloaded():
         raise BadRequestError(message="QQ DEB file not downloaded")
-
-    if qq_manager.is_installed():
+    elif qq_manager.is_installing():
+        raise BadRequestError(message="QQ DEB file is installing")
+    elif qq_manager.is_installed():
         raise BadRequestError(message="QQ already installed")
 
     qq_manager.install()
