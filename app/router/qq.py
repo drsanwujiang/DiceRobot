ec4defd2b1aa9ca71305dc3cf1f2695977d1d6df
Drsanwujiang <drsanwujiang@gmail.com>
2024-10-31 10:44:13 +0800

    Update API

--- app/router/qq.py
+++ app/router/qq.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from ..log import logger
 from ..auth import verify_jwt_token
 from ..exceptions import BadRequestError
-from ..manage import qq_manager
+from ..manage import qq_manager, napcat_manager
 from . import JSONResponse
 
 router = APIRouter(prefix="/qq")
@@ -40,7 +40,7 @@ async def download() -> JSONResponse:
 async def install() -> JSONResponse:
     logger.info("QQ manage request received: install")
 
-    if not qq_manager.is_downloaded():
+    if not qq_manager.is_downloaded() or qq_manager.is_downloading():
         raise BadRequestError(message="QQ DEB file not downloaded")
     elif qq_manager.is_installing():
         raise BadRequestError(message="QQ DEB file is installing")
@@ -48,5 +48,19 @@ async def install() -> JSONResponse:
         raise BadRequestError(message="QQ already installed")
 
     qq_manager.install()
+
+    return JSONResponse()
+
+
+@router.post("/remove", dependencies=[Depends(verify_jwt_token, use_cache=False)])
+async def remove() -> JSONResponse:
+    logger.info("NapCat manage request received: remove")
+
+    if not qq_manager.is_installed():
+        raise BadRequestError(message="QQ not installed")
+    elif napcat_manager.is_installed():
+        raise BadRequestError(message="NapCat not removed")
+
+    qq_manager.remove()
 
     return JSONResponse()
